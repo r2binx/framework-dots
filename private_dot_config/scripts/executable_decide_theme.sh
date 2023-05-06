08bf2d6fb8dd65cf64eddcbbcd84af3e91dc0677
Robin Härle <robin@blckct.io>
2023-05-06 18:40:45 +0200

    Update .config/scripts/decide_theme.sh

--- private_dot_config/scripts/executable_decide_theme.sh
+++ private_dot_config/scripts/executable_decide_theme.sh
@@ -2,10 +2,10 @@
 
 daytime=$(date +%H)
 
-if [ $daytime -ge 21 ] || [ $daytime -le 8 ]; then
-        ~/.config/scripts/theme_toggle.sh dark
-        echo "dark" > /tmp/theme
+if [ $daytime -ge 21 ] || [ $daytime -le 7 ]; then
+	~/.config/scripts/theme_toggle.sh dark
+	echo "dark" >/tmp/theme
 else
-        ~/.config/scripts/theme_toggle.sh light
-        echo "light" > /tmp/theme
+	~/.config/scripts/theme_toggle.sh light
+	echo "light" >/tmp/theme
 fi
