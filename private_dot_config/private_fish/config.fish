8c181a94df40ff6bd88012dad4cec98fcc48e7dc
Robin Härle <robin@blckct.io>
2023-08-29 16:44:44 +0200

    Add .bash_aliases Add .bash_profile Add .bashrc Update .config/kitty/kitty.conf Update .config/fish/config.fish Update .config/fish/fish_variables

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,6 +3,16 @@ source ~/.bash_aliases
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    # source ~/.asdf/asdf.fish
+    source ~/.asdf/asdf.fish
+    
+    set -gx LESSOPEN "| ~/.config/lessfilter %s"
+    set -gx LESS '-cgiRF'
+
+    set fzf_fd_opts --one-file-system --strip-cwd-prefix --follow --hidden --exclude '.git' --exclude 'node_modules' --exclude '.var'
+    set -x fzf_preview_dir_cmd exa -a --icons -1
+    # set -x fzf_preview_dir_cmd 'lsd -LA --color=always -1'
+    set fzf_diff_highlighter delta
+
     starship init fish | source
 end
+
