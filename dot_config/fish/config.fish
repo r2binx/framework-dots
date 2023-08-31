2a19bea9471aa897e12b977fa8b3478cd1c2988d
Robin Härle <robin@blckct.io>
2023-08-31 13:55:15 +0200

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -10,7 +10,7 @@ if status is-interactive
     set -gx LESSOPEN "| ~/.config/lessfilter %s"
     set -gx LESS '-cgiRF'
 
-    set -gx FZF_DEFAULT_OPTS --bind 'ctrl-y:execute-silent(printf {} | cut -f 2- | wl-copy --trim-newline)' --bind '?:toggle-preview' --bind 'ctrl-a:toggle-all' --cycle --layout=reverse --border --preview-window=wrap --marker='*' --height=80% --min-height=20
+    set -gx FZF_DEFAULT_OPTS "--bind 'ctrl-y:execute-silent(printf {} | cut -f 2- | wl-copy --trim-newline)' --bind '?:toggle-preview' --bind 'ctrl-a:toggle-all' --cycle --layout=reverse --border --preview-window=wrap --marker='*' --height=80% --min-height=20"
     set fzf_fd_opts --one-file-system --strip-cwd-prefix --follow --hidden --exclude '.git' --exclude 'node_modules' --exclude '.var'
     set -x fzf_preview_dir_cmd exa -a --group-directories-first --icons -1
     # set -x fzf_preview_dir_cmd 'lsd -LA --color=always -1'
