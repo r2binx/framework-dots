8253e9fed0e54d607bb6d025e57b672da2a50338
Robin Härle <robin@blckct.io>
2023-11-20 11:43:35 +0100

    Remove .chezmoiscripts/libadwaita4.sh Update .config/sway/config Update .config/sway/env Update .config/waybar/config Remove 00-install-hwdb.sh Add 00-symlink_overrides.sh

--- run_once_after_00-install-hwdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-sudo ln -s "${HOME}/.local/share/udev/hwdb.d/gsx-1000.hwdb" /etc/udev/hwdb.d/gsx-1000.hwdb
---
+++ run_once_after_00-symlink_overrides.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -eu
+
+has_cmd() {
+	command -v "$1" >/dev/null 2>&1
+
+}
+
+if [ ! -f /etc/udev/hwdb.d/gsx-1000.hwdb ]; then
+	sudo ln -s "${HOME}/.local/share/udev/hwdb.d/gsx-1000.hwdb" /etc/udev/hwdb.d/gsx-1000.hwdb
+fi
+
+if has_cmd flatpak; then
+	set -x
+	sudo flatpak override --filesystem=xdg-config/gtk-3.0:ro --filesystem=xdg-config/gtk-4.0:ro --filesystem=~/.local/share/themes:ro --filesystem=/usr/share/themes:ro --filesystem=/usr/share/icons:ro
+fi
