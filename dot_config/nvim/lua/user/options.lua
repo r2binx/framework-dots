12a23085560434527a62bbd4a03bb4c04c34352a
Robin Härle <robin@blckct.io>
2024-03-06 11:23:36 +0100

    Update .config/nvim/lua/user/options.lua Update .config/nvim/lua/user/plugins/community.lua

--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -1,34 +1,34 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
 return {
-	opt = {
-		-- set to true or false etc.
-		-- autochdir = true, -- changes cwd to buffer file
-		relativenumber = true, -- sets vim.opt.relativenumber
-		number = true, -- sets vim.opt.number
-		spell = false, -- sets vim.opt.spell
-		signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-		wrap = true, -- sets vim.opt.wrap
-		-- for neovide
-		guifont = { "Comic Code Ligatures", ":h12" },
-	},
-	g = {
-		mapleader = " ", -- sets vim.g.mapleader
-		autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-		cmp_enabled = true, -- enable completion at start
-		autopairs_enabled = true, -- enable autopairs at start
-		diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-		icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-		ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-		resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
-		-- neovide specific options
-		neovide_refresh_rate = 60,
-		neovide_refresh_rate_idle = 5,
-		neovide_hide_mouse_when_typing = true,
-		neovide_touch_deadzone = 3.0,
-		neovide_cursor_animation_length = 0.08,
-		neovide_scroll_animation_length = 0,
-		neovide_cursor_antialiasing = false,
-	},
+  opt = {
+    -- set to true or false etc.
+    -- autochdir = true, -- changes cwd to buffer file
+    relativenumber = true, -- sets vim.opt.relativenumber
+    number = true, -- sets vim.opt.number
+    spell = false, -- sets vim.opt.spell
+    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+    wrap = true, -- sets vim.opt.wrap
+    -- for neovide
+    guifont = { "Comic Code Ligatures", ":h12" },
+  },
+  g = {
+    mapleader = " ", -- sets vim.g.mapleader
+    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+    cmp_enabled = true, -- enable completion at start
+    autopairs_enabled = true, -- enable autopairs at start
+    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+    resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+    -- neovide specific options
+    neovide_refresh_rate = 60,
+    neovide_refresh_rate_idle = 5,
+    neovide_hide_mouse_when_typing = true,
+    neovide_touch_deadzone = 3.0,
+    neovide_cursor_animation_length = 0.08,
+    neovide_scroll_animation_length = 0,
+    neovide_cursor_antialiasing = false,
+  },
 }
 -- If you need more control, you can use the function()...end notation
 -- return function(local_vim)
