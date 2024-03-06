21f3baf8037c6250d4ec84b71e69590193f624ab
Robin Härle <robin@blckct.io>
2024-03-06 11:16:40 +0100

    Update .bash_profile Update .config/code-flags.conf Update .config/environment.d/sway.conf Update .config/kitty/kitty.conf Update .config/nvim/lua/user/options.lua Update .config/nvim/lua/user/plugins/community.lua Update .config/pacman/makepkg.conf Update .config/sway/autostart.conf Update .config/sway/config Update .config/swaync/config.json Update .config/swaync/configSchema.json Update .config/swaync/style.css Update .config/waybar/config Update .config/waybar/get_theme.sh Update .config/waybar/style.css Update .config/wofi/style.css Add .fleet/catppuccin-frappe.json Add .fleet/catppuccin-latte.json Add .fleet/catppuccin-macchiato.json Add .fleet/catppuccin-mocha.json Update .local/share/udev/hwdb.d/gsx-1000.hwdb

--- dot_config/nvim/lua/user/plugins/community.lua
+++ dot_config/nvim/lua/user/plugins/community.lua
@@ -1,74 +1,77 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+	-- Add the community repository of plugin specifications
+	"AstroNvim/astrocommunity",
+	-- example of importing a plugin, comment out to use it or add your own
+	-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  {
-    "catppuccin",
-    opts = {
-      flavour = "frappe",
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      dim_inactive = {
-        enabled = true,
-        shade = "dark",
-        percentage = 0.15,
-      },
-      integrations = {
-        indent_blankline = {
-          enabled = true,
-          scope_color = "",
-          colored_indent_levels = true,
-        },
-      },
-    },
-  },
-  { import = "astrocommunity.indent.indent-blankline-nvim" },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    opts = {
-      scope = {
-        enabled = true,
-        show_start = true,
-        show_end = false,
-        highlight = { "Function", "Label" },
-      },
-      indent = {
-        highlight = {
-          "RainbowRed",
-          "RainbowYellow",
-          "RainbowBlue",
-          "RainbowOrange",
-          "RainbowGreen",
-          "RainbowViolet",
-          "RainbowCyan",
-        },
-      },
-    },
-  },
-  { import = "astrocommunity.diagnostics.trouble-nvim" },
-  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
-  { import = "astrocommunity.editing-support.todo-comments-nvim" },
-  { import = "astrocommunity.editing-support.nvim-regexplainer" },
-  { import = "astrocommunity.register.nvim-neoclip-lua" },
-  { import = "astrocommunity.motion.mini-surround" },
-  { import = "astrocommunity.motion.nvim-spider" },
-  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.vue" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.toml" },
-  { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.colorscheme.catppuccin" },
+	{
+		"catppuccin",
+		opts = {
+			flavour = "frappe",
+			background = {
+				light = "latte",
+				dark = "mocha",
+			},
+			dim_inactive = {
+				enabled = true,
+				shade = "dark",
+				percentage = 0.15,
+			},
+			integrations = {
+				indent_blankline = {
+					enabled = true,
+					scope_color = "",
+					colored_indent_levels = true,
+				},
+			},
+		},
+	},
+	{ import = "astrocommunity.indent.indent-blankline-nvim" },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		opts = {
+			scope = {
+				enabled = true,
+				show_start = true,
+				show_end = false,
+				highlight = { "Function", "Label" },
+			},
+			indent = {
+				highlight = {
+					"RainbowRed",
+					"RainbowYellow",
+					"RainbowBlue",
+					"RainbowOrange",
+					"RainbowGreen",
+					"RainbowViolet",
+					"RainbowCyan",
+				},
+			},
+		},
+	},
+	{ import = "astrocommunity.diagnostics.trouble-nvim" },
+	{ import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+	{ import = "astrocommunity.editing-support.todo-comments-nvim" },
+	{ import = "astrocommunity.editing-support.nvim-regexplainer" },
+	{ import = "astrocommunity.register.nvim-neoclip-lua" },
+	{ import = "astrocommunity.motion.mini-surround" },
+	{ import = "astrocommunity.motion.nvim-spider" },
+	{ import = "astrocommunity.motion.leap-nvim" },
+	{ import = "astrocommunity.markdown-and-latex.glow-nvim" },
+	{ import = "astrocommunity.project.project-nvim" },
+	{ import = "astrocommunity.pack.bash" },
+	{ import = "astrocommunity.pack.docker" },
+	{ import = "astrocommunity.pack.cpp" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.typescript" },
+	{ import = "astrocommunity.pack.vue" },
+	{ import = "astrocommunity.pack.go" },
+	{ import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.pack.toml" },
+	{ import = "astrocommunity.pack.yaml" },
+	{ import = "astrocommunity.pack.html-css" },
+	{ import = "astrocommunity.pack.lua" },
 
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+	-- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
--- dot_config/waybar/executable_get_theme.sh
+++ dot_config/waybar/executable_get_theme.sh
@@ -2,6 +2,9 @@
 
 [ -f /tmp/theme ] && read -r theme </tmp/theme || theme=$([ "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'default'" ] || [ "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-light'" ] && echo "light" || echo "dark")
 
-brightness=$(light -s sysfs/backlight/intel_backlight -G)
+max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+actual_brightness=$(cat /sys/class/backlight/intel_backlight/actual_brightness)
+
+brightness=$(echo "($actual_brightness/$max_brightness)*100" | bc -l)
 
 jq --unbuffered --compact-output --arg theme "${theme}" --arg brightness "${brightness}" -n '{"class":$theme, "alt":$theme,"percentage":$brightness|tonumber|floor}'
