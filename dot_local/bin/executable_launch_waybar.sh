3af44254be38e768a624c3d3325e2bc65f8c5a9d
Robin Härle <robin@blckct.io>
2024-12-10 11:36:58 +0100

    Update .config/waybar/config Update .local/bin/launch_waybar.sh Add .config/waybar/modify_config

--- dot_local/bin/executable_launch_waybar.sh
+++ dot_local/bin/executable_launch_waybar.sh
@@ -2,22 +2,22 @@
 
 BAR_ID=''
 while getopts 'w:b:' flag; do
-	case "${flag}" in
-	b) BAR_ID="${OPTARG}" ;;
-	*)
-		printf "${@}"
-		exit 1
-		;;
-	esac
+  case "${flag}" in
+  b) BAR_ID="${OPTARG}" ;;
+  *)
+    printf "${@}"
+    exit 1
+    ;;
+  esac
 done
 
 SDIR="${HOME}/.config/waybar"
 for i in /sys/class/hwmon/hwmon*/temp*_input; do
-	if [ "$(<"$(dirname "${i}")"/name): $(cat "${i%_*}"_label 2>/dev/null || basename "${i%_*}")" = "coretemp: Package id 0" ]; then
-		export HWMON_PATH="${i}"
-		break
-	fi
+  if [ "$(<"$(dirname "${i}")"/name): $(cat "${i%_*}"_label 2>/dev/null || basename "${i%_*}")" = "coretemp: Package id 0" ]; then
+    export HWMON_PATH="${i}"
+    break
+  fi
 done
-sed -i "/hwmon-path/c\ \ \ \ \"hwmon-path\": \"${HWMON_PATH}\"," "${SDIR}/config"
 
+sed -i "s|{{HWMON_PATH}}|${HWMON_PATH}|" "${SDIR}/config"
 /usr/bin/waybar -c "${SDIR}/config" -b "${BAR_ID}"
