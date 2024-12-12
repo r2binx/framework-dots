7c476956bdb4ac707e482618ff45f79ef8aaf2f8
Robin Härle <robin@blckct.io>
2024-12-12 19:50:57 +0100

    Update .config/nvim/lua/plugins/core.lua Update .config/nvim/lua/plugins/extras.lua

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -1,5 +1,3 @@
--- local hooks = require("ibl.hooks")
--- hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 return {
   {
     "LazyVim/LazyVim",
@@ -16,6 +14,43 @@ return {
           enabled = true,
           colored_indent_levels = true,
         },
+      },
+    },
+  },
+  {
+    "chrisgrieser/nvim-spider",
+    keys = {
+      {
+        "w",
+        function()
+          require("spider").motion("w")
+        end,
+        desc = "Next word",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "e",
+        function()
+          require("spider").motion("e")
+        end,
+        desc = "Next end of word",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "b",
+        function()
+          require("spider").motion("b")
+        end,
+        desc = "Previous word",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "ge",
+        function()
+          require("spider").motion("ge")
+        end,
+        desc = "Previous end of word",
+        mode = { "n", "o", "x" },
       },
     },
   },
--- dot_config/nvim/lua/plugins/extras.lua
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -11,5 +11,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.vue" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
   { import = "lazyvim.plugins.extras.lang.docker" },
 }
